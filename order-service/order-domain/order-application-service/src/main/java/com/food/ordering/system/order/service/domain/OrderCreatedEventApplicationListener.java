e672190e3ae0386026e37aa2d1c6d94656591c30
eduardodcs <eddcesilva@gmail.com>
2024-02-13 11:52:14 -0300

    Publish with transactional listener

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderCreatedEventApplicationListener.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderCreatedEventApplicationListener.java
@@ -1,6 +1,10 @@
 package com.food.ordering.system.order.service.domain;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
+import com.food.ordering.system.order.service.domain.ports.output.message.publisher.payment.OrderCreatedPaymentRequestMessagePublisher;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -8,6 +12,16 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class OrderCreatedEventApplicationListener {
 
-	
+	 private final OrderCreatedPaymentRequestMessagePublisher orderCreatedPaymentRequestMessagePublisher;
+
+	    public OrderCreatedEventApplicationListener(OrderCreatedPaymentRequestMessagePublisher
+	                                                        orderCreatedPaymentRequestMessagePublisher) {
+	        this.orderCreatedPaymentRequestMessagePublisher = orderCreatedPaymentRequestMessagePublisher;
+	    }
+
+	   @TransactionalEventListener
+	   void process(OrderCreatedEvent orderCreatedEvent) {
+	        orderCreatedPaymentRequestMessagePublisher.publish(orderCreatedEvent);
+	   }	
 	
 }
